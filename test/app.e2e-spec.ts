74979b97ed8591e6f08a4a2d0695362fb6554861
choewy <choewy32@gmail.com>
2024-03-07 00:04:57 +0900

    feature/e2e setting

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,12 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
-import * as request from 'supertest';
-
-import { AppModule } from './../src/app.module';
+import { AppModule } from 'src/app.module';
+import request from 'supertest';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -18,5 +17,9 @@ describe('AppController (e2e)', () => {
 
   it('/ (GET)', () => {
     return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
