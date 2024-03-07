470b4136f8e2d9b3759c8b6cdf171931d749b8a7
choewy <choewy32@gmail.com>
2024-03-07 19:43:37 +0900

    feature/health libs module

--- libs/health/src/health.module.ts
+++ libs/health/src/health.module.ts
@@ -1,8 +1,14 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
+
+import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
+import { HttpPingIndicator } from './http-ping.indicator';
 
 @Module({
-  providers: [HealthService],
-  exports: [HealthService],
+  imports: [TerminusModule, HttpModule],
+  controllers: [HealthController],
+  providers: [HealthService, HttpPingIndicator],
 })
-export class HealthModule {}
+export class HealthLibsModule {}
--- libs/health/src/health.service.spec.ts
+++ libs/health/src/health.service.spec.ts
@@ -1,12 +1,17 @@
+import { HttpModule } from '@nestjs/axios';
+import { TerminusModule } from '@nestjs/terminus';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { HealthService } from './health.service';
+import { HttpPingIndicator } from './http-ping.indicator';
 
 describe('HealthService', () => {
   let service: HealthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HealthService],
+      imports: [TerminusModule, HttpModule],
+      providers: [HttpPingIndicator, HealthService],
     }).compile();
 
     service = module.get<HealthService>(HealthService);
